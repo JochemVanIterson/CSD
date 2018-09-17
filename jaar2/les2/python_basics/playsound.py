c0c8240e394ebd67bf3f08ae33532275416a040d
JochemVanIterson <jochem@audioware.nl>
2018-09-17 14:18:19 +0200

    comments bij les 2

--- jaar2/les2/python_basics/playsound.py
+++ jaar2/les2/python_basics/playsound.py
@@ -1,4 +1,4 @@
-# command: python3 playsound.py 4 1 0.5 1.5 0.5 120
+# command: python3 playsound.py 4 1 .5 1.5 1 120
 
 # ------------------------------------------- Imports ------------------------------------------- #
 import os, fnmatch
@@ -7,9 +7,10 @@ import simpleaudio as sa
 import time
 
 # ------------------------------------------- Variables ----------------------------------------- #
-numPlaybackTimes = int(sys.argv[1])
-sequence = sys.argv[2:2+numPlaybackTimes]
-BPM = int(sys.argv[-1])
+# ['playsound.py', '4', '1', '.5', '1.5', '1', '150']
+numPlaybackTimes = int(sys.argv[1]) # take second var from array
+sequence = sys.argv[2:2+numPlaybackTimes] # take from third var plus 'numPlaybackTimes' from array
+BPM = int(sys.argv[-1]) # take last from array
 print('numPlaybackTimes', numPlaybackTimes)
 print('sequence', str(sequence))
 print('BPM', BPM)
