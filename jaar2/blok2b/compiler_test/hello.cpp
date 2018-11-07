2bb2a0c0cebff5132be8d6052457546ed6b08689
JochemVanIterson <jochem@audioware.nl>
2018-11-07 21:03:06 +0100

    Compiler test

---
+++ jaar2/blok2b/compiler_test/hello.cpp
@@ -0,0 +1,14 @@
+/*
+* Super basic C++ hello world
+*
+* ... but hey.. what's the purpose of hello world?
+*
+* ... that's right.. being super basic!
+*
+*/
+#include <iostream>
+
+int main() {
+ std::cout << "Hello World :-)" << std::endl;
+ return 0;
+}
