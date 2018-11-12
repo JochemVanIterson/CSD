a076bc8aceba38ab2064d0060c00c6d68db8a080
JochemVanIterson <jochem@audioware.nl>
2018-11-12 11:15:54 +0100

    Now uses own dbgPrint lib

---
+++ jaar2/blok2b/opdracht1/src/dbgPrint/dbgPrint.h
@@ -0,0 +1,5 @@
+class dbgPrint {
+  public:
+    static void d(std::string key, std::string message);
+    static void e(std::string key, std::string message);
+};
--- jaar2/blok2b/opdracht1/src/main.cpp
+++ jaar2/blok2b/opdracht1/src/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 
+#include "dbgPrint/dbgPrint.cpp"
 #include "instrument.cpp"
 
 int main() {
-  std::cout << "main()" << std::endl; // stringout
   Instrument inst1 = Instrument("test1");
   Instrument inst2 = Instrument("test2");
 
-  std::cout << "\nStatic playback" << std::endl; // stringout
+  dbgPrint::d("", "Static playback");
+  // std::cout << "\nStatic playback" << std::endl; // stringout
   inst1.makeSound();
   inst2.makeSound();
   inst1.makeSound();
   inst2.makeSound();
 
-  std::cout << "\nRoll playback" << std::endl; // stringout
+  dbgPrint::d("", "Roll playback");
+  // std::cout << "\nRoll playback" << std::endl; // stringout
   inst1.roll(5);
   inst2.roll(2);
   return 0;
