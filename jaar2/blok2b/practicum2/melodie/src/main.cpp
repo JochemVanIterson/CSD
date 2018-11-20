ce71828af89eb84036fc506cc0a6e65ec07ce483
JochemVanIterson <jochem@audioware.nl>
2018-11-20 14:52:41 +0100

    Practicum 2 Melody

---
+++ jaar2/blok2b/practicum2/melodie/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+#include "melody.hpp"
+
+int main(){
+  std::cout << "main()" << std::endl;
+
+  Melody melody;
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // leeg -> -1
+  melody.addNote(34);
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 34
+  melody.addNote(47);
+  melody.addNote(95);
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 47
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 95
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // leeg -> -1
+  melody.addNote(54);
+  melody.addNote(73);
+  melody.addNote(32);
+  melody.addNote(87);
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 54
+  melody.addNote(36);
+  melody.printList();
+  melody.addNote(47);
+
+  melody.addNote(97);
+  melody.addNote(62);
+  melody.addNote(15);
+  melody.printList();
+  melody.addNote(58);
+  melody.addNote(89); // Error
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 73
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 32
+  std::cout << "melody.getNote(): " << melody.getNote() << std::endl; // 87
+  melody.addNote(10);
+  melody.addNote(11);
+
+  melody.printList();
+
+  return 0;
+}
---
+++ jaar2/blok2b/practicum2/melodie/src/melody.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include "melody.hpp"
+
+// constructor and destructor
+Melody::Melody(){
+  std::cout << "Melody::Melody()" << std::endl;
+  readIndex = 0;
+  writeIndex = 0;
+}
+Melody::~Melody(){
+  std::cout << "Melody::~Melody()" << std::endl;
+}
+
+// Functions
+int Melody::getNote(){
+  std::cout << readIndex << ":" << writeIndex << " ";
+  if(readIndex!=(writeIndex)){
+    int value = notes[readIndex];
+    readIndex = (readIndex+1) % NUM_NOTES;
+    return value;
+  } else {
+    return -1;
+  }
+}
+void Melody::addNote(int note){
+  std::cout << readIndex << ":" << writeIndex << std::endl;
+  if(writeIndex!=(readIndex-1)){
+    notes[writeIndex] = note;
+    writeIndex = (writeIndex+1) % NUM_NOTES;
+  } else {
+    std::cout << "void Melody::addNote(int note): Couldn't add note " << note << std::endl;
+  }
+}
+void Melody::printList(){
+  std::cout << "void Melody::printList():" << std::endl;
+
+  std::cout << "\treadIndex: " << readIndex << "-> " << notes[readIndex] << std::endl;
+  std::cout << "\twriteIndex: " << writeIndex << "-> " << notes[writeIndex] << std::endl;
+
+  int walker = 0;
+  while(walker<NUM_NOTES) {
+    std::cout << "\t" << walker << ": "<< notes[walker] << std::endl;
+    walker++;
+  }
+
+}
